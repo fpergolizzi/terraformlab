1ff43870d3a7014f8530794477d7de4b63e5dba1
Frank Pergolizzi <fpergolizzi@hotmail.com>
2022-05-06 15:24:32 -0400

    Second commit

--- main.tf
+++ main.tf
@@ -3,10 +3,10 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-0022f774911c1d690"
+    ami           = "ami-09d56f8956ab235b3"
     instance_type = "t2.micro"
 
     tags = {
-        Name = "terraform-example"
+        Name = "terraform-ec2-example"
     }
 }
